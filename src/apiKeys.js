7f58145530209d332a53b5b4f63e26af89ef25f8
foody12356 <98463357+foody12356@users.noreply.github.com>
2023-05-29 20:06:43 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "a950bf677197029b69c75bc54cff0557",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
